f79c5755895868274b82e637a398a49766a55e4c
yanethvargasherhuay <y4neth_182@hotmail.com>
2015-03-24 23:28:41 -0500

    subiendo proyecto wcfresful

---
+++ Solution1/WcfService1/IWCFRest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace WcfService1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IWCFRest" en el código y en el archivo de configuración a la vez.
+
+    [DataContract]
+    public class User 
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+    }
+    [ServiceContract]
+    public interface IWCFRest
+    {
+        [OperationContract]
+        [WebInvoke(Method="GET",ResponseFormat=WebMessageFormat.Json,UriTemplate="show/json/{name}")]
+        List<User> ShowJson(string name);
+
+        [OperationContract]
+        [WebInvoke(Method="GET",ResponseFormat=WebMessageFormat.Xml,UriTemplate="show/xml/{name}")]
+        List<User> ShowXml(string name);
+        
+    }
+}
---
+++ Solution1/WcfService1/WCFRest.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace WcfService1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "WCFRest" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione WCFRest.svc o WCFRest.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WCFRest : IWCFRest
+    {
+        private List<User> UsersFromDataBase = new List<User>() 
+        {
+            new User(){id=1,Name="Juan",Email="Juan@gmail.com"},
+            new User(){id=2,Name="Pedro",Email="Pedro@gmail.com"},
+            new User (){id=3,Name="Magaly",Email="Magaly@gmail.com"}
+
+        };
+        public List<User> ShowJson(string name) 
+        {
+            return GetUsersByName(name);
+        }
+        public List<User> ShowXml(string name) 
+        {
+            return GetUsersByName(name);
+        }
+
+        private List<User> GetUsersByName(string name) 
+        {
+            return this.UsersFromDataBase.Where(w => w.Name == name).Select(s => s).ToList<User>();
+        }
+    }
+}
